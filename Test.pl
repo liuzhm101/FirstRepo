e3ff4ea4307c1c065ced941b3dbc4eabb0a47824
liuzhm101 <liuzhm101@163.com>
2023-12-18 17:31:01 +0800

    first

---
+++ Test.pl
@@ -0,0 +1,12 @@
+# First Perl Program
+
+print "hello world!\n";
+print "my name is zhiming\n";
+print "这是我的第一个程序\n";
+
+$a=56;
+$b=58e10*3;  # e前面不能有*号，默认就是乘的关系
+$c=$a*$b;
+print $a . "\n";  # . 是字符串连接符号
+print $b . "\n";
+print $c . "\n";
